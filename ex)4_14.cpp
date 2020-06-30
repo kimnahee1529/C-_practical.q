c837f87677c9e85c929bc83579e1255a1d54c39b
kimnahee1529 <65105035+kimnahee1529@users.noreply.github.com>
2020-06-30 14:56:25 +0900

    Create ex)4_14.cpp

---
+++ ex)4_14.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << "덧셈문자열 입력: ";//7+23+5+100+25
+	string s;
+	getline(cin, s, '\n');
+	//int sum = 0; 
+
+	int startIndex=0,fIndex=0,sum = 0;
+	while(true){
+		fIndex= s.find('+', startIndex);//startIndex(처음은 0)부터 +검색 : fIndex=1
+		if (fIndex == -1) {//'+' 발견X ->마지막 숫자일 때
+			string part = s.substr(startIndex);//startIndex에서 끝까지 서브스트링으로 만들기
+			if (part == "")break;
+			cout << part << endl;
+			sum += stoi(part);
+			break;
+		}
+		int count = fIndex - startIndex;
+		string part = s.substr(startIndex, count);
+		cout << part << endl;
+		sum += stoi(part);
+		startIndex = fIndex + 1;
+	}
+	cout << "숫자들의 합은 " << sum<<endl;
+}
