005f55dc9ad32ab8640b14fdfebcfe02a40b3709
kimnahee1529 <65105035+kimnahee1529@users.noreply.github.com>
2020-07-17 19:47:03 +0900

    Update 4_9.cpp

--- 4_9.cpp
+++ 4_9.cpp
@@ -6,14 +6,11 @@ class Person {
 	string name;
 	string tel;
 public:
-	Person();
+	Person(){;}
 	string getName() { return name; }
 	string getTel() { return tel;}
 	void set(string name, string tel);
 };
-Person::Person() {
-	
-}
 void Person::set(string name, string tel) {
 	this->name = name; 
 	this->tel = tel;
